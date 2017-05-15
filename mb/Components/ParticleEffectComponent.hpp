57d6f3b000cc3ea9ea5f2583ae2e7ac164f46123
maldicion069 <ccrisrober@gmail.com>
2017-05-15 15:46:19 +0200

    Added ParticleAttrArray

--- mb/Components/ParticleEffectComponent.hpp
+++ mb/Components/ParticleEffectComponent.hpp
@@ -1,30 +1,87 @@
 #include "Component.hpp"
 
+#include <bitset>
+
 namespace mb
 {
+  enum ParticleAttrType
+  {
+    POSITION,
+    VELOCITY,
+    COLOR
+  };
+
+  typedef unsigned int ParticleID;
+
+  template< typename T >
+  class ParticleAttrArray
+  {
+  public:
+    template< typename T >
+    T &operator[]( unsigned int idx )
+    {
+      return static_cast< T* >( _data.getData( ) )[ idx ];
+    }
+    template< typename T >
+    const T &operator[]( unsigned int idx ) const
+    {
+      return static_cast< T* >( _data.getData( ) )[ idx ];
+    }
+    template< typename T >
+    T* getData( void )
+    {
+      return static_cast< T* >( _data.getData( ) );
+    }
+    template< typename T >
+    const T* getData( void ) const
+    {
+      return static_cast< T* >( _data.getData( ) );
+    }
+    virtual unsigned int getCount( void ) const
+    {
+      return _data.size( );
+    }
+
+    virtual void reset( unsigned int count )
+    {
+      _data.resize( count );
+    }
+
+    virtual void swap( ParticleID a, ParticleID b )
+    {
+      std::swap( _data[ a ], _data[ b ] );
+    }
+  protected:
+    std::vector< T > _data;
+  };
+
   class Particle
   {
+  public:
+    Particle( )
+    {
+      _alive.reset( 1000 );
+    }
+    ///std::map<ParticleAttrType, std::shared_ptr<ParticleAttrArray>> _particleData;
+    std::bitset<1> _alive;
   };
   typedef std::shared_ptr< Particle > ParticlePtr;
   class ParticleUpdater
   {
   public:
-    virtual void configure( void ) = 0;
-    virtual void update( void ) = 0;
+    virtual void update( const float& ) = 0;
   };
   typedef std::shared_ptr< ParticleUpdater > ParticleUpdaterPtr;
   class ParticleRenderer
   {
   public:
-    virtual void configure( void ) = 0;
-    virtual void update( void ) = 0;
+    virtual void update( const float& ) = 0;
   };
   typedef std::shared_ptr< ParticleRenderer > ParticleRendererPtr;
   class ParticleGenerator 
   {
   public:
-    virtual void configure( void ) = 0;
-    virtual void generate( void ) = 0;
+    virtual void generate( const float& ) = 0;
   };
   typedef std::shared_ptr< ParticleGenerator > ParticleGeneratorPtr;
 
@@ -72,25 +129,25 @@ namespace mb
     std::vector< ParticleUpdaterPtr > _updaters;
     std::vector< ParticleRendererPtr > _renderers;
   protected:
-    void updateGenerators( )
+    void updateGenerators( const float& dt )
     {
       for ( auto& gen : _generators )
       {
-        gen->generate( );
+        gen->generate( dt );
       }
     }
-    void updateUpdaters( )
+    void updateUpdaters( const float& dt )
     {
       for ( auto& upd : _updaters )
       {
-        upd->update( );
+        upd->update( dt );
       }
     }
-    void updateRenderers( )
+    void updateRenderers( const float& dt )
     {
       for ( auto& rend : _renderers )
       {
-        rend->update( );
+        rend->update( dt );
       }
     }
   };
