e69dfbf3dd2747ce3c6a5751cd06978b9436cc85
Cristian Rodríguez <ccrisrober@gmail.com>
2017-07-13 12:55:39 +0200

    Transform Feedback (WIP)

--- examples/demos.cpp
+++ examples/demos.cpp
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2017, Monkey Brush
+ * All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
 #include <mb/mb.h>
 #include <routes.h>
 
@@ -7,24 +26,81 @@ int main( )
   mb::Window* window = new mb::GLFWWindow2( mb::WindowParams( 512, 512 ) );
   window->init( );
 
-  mb::PostProcessMaterial pp(R"(
-#version 430
-in vec2 uv;
-out vec4 fragColor;
-uniform sampler2D targetTex;
-void main( )
-{
-  fragColor = vec4( texture( targetTex, uv ).rgb, 1.0 );
-})");
+  // mb::Renderer* r = new mb::OpenGLRenderer( );
 
-  mb::Texture2D* t = mb::Texture2D::loadFromImage( "earth/earth_diffuse.jpg" );
+  const GLchar* vertexShaderSrc = R"(
+    #version 330
+    in float inValue;
+    out float outValue;
 
-  mb::Renderer* r = new mb::OpenGLRenderer( );
+    void main( )
+    {
+      outValue = sqrt( inValue );
+    }
+  )";
 
-  mb::Clock clockTime;
+  const GLchar* feedbackVaryings[] = { "outValue" };
+  mb::Program p;
+  p.loadVertexShaderFromText( vertexShaderSrc );
+
+  p.create( );
+  p.feedbackVarying( feedbackVaryings, 1, GL_INTERLEAVED_ATTRIBS );
+  p.link( );
+  p.autocatching( );
+
+  p.use( );
+
+  mb::TransformFeedback ttf;
+
+  // Create VAO
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // Create input VBO and vertex format
+  GLfloat data[] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+
+  glEnableVertexAttribArray(p.attribute( "inValue" ) );
+  glVertexAttribPointer(p.attribute( "inValue" ) , 1, GL_FLOAT, GL_FALSE, 0, 0);
+
+  // Create transform feedback buffer
+  GLuint tbo;
+  glGenBuffers(1, &tbo);
+  glBindBuffer(GL_ARRAY_BUFFER, tbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(data), nullptr, GL_STATIC_READ);
+
+  // Perform feedback transform
+  glEnable(GL_RASTERIZER_DISCARD);
+
+  glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, tbo);
+
+  glBeginTransformFeedback(GL_POINTS);
+      glDrawArrays(GL_POINTS, 0, 5);
+  glEndTransformFeedback();
+
+  glDisable(GL_RASTERIZER_DISCARD);
+
+  glFlush();
+
+  // Fetch and print results
+  GLfloat feedback[5];
+  glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, sizeof(feedback), feedback);
+
+  printf("%f %f %f %f %f\n", feedback[0], feedback[1], feedback[2], feedback[3], feedback[4]);
+
+  glDeleteBuffers(1, &tbo);
+  glDeleteBuffers(1, &vbo);
+
+  glDeleteVertexArrays(1, &vao);
+
+
+  /*mb::Clock clockTime;
   clockTime.reset( );
-
-  pp.addUniform( "targetTex", std::make_shared< mb::TextureUniform >( t ) );
 
   while ( window->isRunning( ) )
   {
@@ -37,14 +113,9 @@ void main( )
     }
 
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    clockTime.tick( );
-
-    pp.use( );
-
-    r->drawScreenQuad( );
 
     window->swapBuffers( );
-  }
+  }*/
 
   return 0;
 }
---
+++ mb/OpenGL/TransformFeedback.cpp
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2017, Monkey Brush
+ * All rights reserved.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#include "TransformFeedback.hpp"
+#include "../Includes.hpp"
+
+namespace mb
+{
+  TransformFeedback::TransformFeedback( void )
+  {
+    glCreateTransformFeedbacks(1, &_handler);
+  }
+  TransformFeedback::~TransformFeedback( void )
+  {
+    glDeleteTransformFeedbacks(1, &_handler);
+  }
+  void TransformFeedback::bind( void )
+  {
+    glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, _handler);
+  }
+  void TransformFeedback::unbind( void )
+  {
+    glBindTransformFeedback(GL_TRANSFORM_FEEDBACK, 0);
+  }
+  void TransformFeedback::begin(unsigned int mode)
+  {
+    glBeginTransformFeedback(mode);
+  }
+  void TransformFeedback::beginPoints( void )
+  {
+    begin(GL_POINTS);
+  }
+  void TransformFeedback::beginLines( void )
+  {
+    begin(GL_LINES);
+  }
+  void TransformFeedback::beginTriangles( void )
+  {
+    begin(GL_TRIANGLES);
+  }
+  void TransformFeedback::end( void )
+  {
+    glEndTransformFeedback();
+  }
+  void TransformFeedback::pause( void )
+  {
+    glPauseTransformFeedback();
+  }
+  void TransformFeedback::resume( void )
+  {
+    glResumeTransformFeedback();
+  }
+  void TransformFeedback::varyings(const mb::Program* prog,
+    const std::vector<const char*>& varyings, unsigned int bufferMode)
+  {
+    glTransformFeedbackVaryings(prog->program(),
+      varyings.size(), &varyings[0], bufferMode);
+  }
+  std::vector<float> TransformFeedback::extractData(unsigned int numElems) const
+  {
+    std::vector<float> feedback(numElems);
+    glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0,
+      sizeof(feedback), &feedback[0]);
+    return feedback;
+  }
+}
