61010ade77c0b763fc0f34b71e95a47ea6393d3b
rjarillo <r.jarillo@alumnos.urjc.es>
2017-07-18 13:36:35 +0200

    Creating geometry to Capsule primitive.
     + Adding image with normals to torus (for donut demo).
     + Minor changes (adapting code).

--- mb/Primitives/MeshPrimitive.cpp
+++ mb/Primitives/MeshPrimitive.cpp
@@ -24,7 +24,9 @@
 
 namespace mb
 {
-  MeshPrimitive::MeshPrimitive( const std::string& fileName )
+  MeshPrimitive::MeshPrimitive(const std::string& fileName , Type type,
+                               TDrawType typeDraw )
+  : Primitive( type, typeDraw )
   {
     mb::ObjLoader::loadObj( fileName );
 
@@ -37,45 +39,6 @@ namespace mb
     // Indices
     indices = mb::ObjLoader::indexData;
 
-    uint32_t VBO[ 4 ];
-    glGenVertexArrays( 1, &VAO );
-    glBindVertexArray( VAO );
-    glGenBuffers( 4, VBO );
-
-    glBindVertexArray( VAO );
-
-    //Vertices
-    glBindBuffer( GL_ARRAY_BUFFER, VBO[ 0 ] );
-    glBufferData( GL_ARRAY_BUFFER, sizeof( Vector3 ) *vertices.size( ), vertices.data( ), GL_STATIC_DRAW );
-    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 0, 0 );
-    glEnableVertexAttribArray( 0 );
-
-    //Normals
-    glBindBuffer( GL_ARRAY_BUFFER, VBO[ 1 ] );
-    glBufferData( GL_ARRAY_BUFFER, sizeof( Vector3 ) *normals.size( ), normals.data( ), GL_STATIC_DRAW );
-    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 0, 0 );
-    glEnableVertexAttribArray( 1 );
-
-    //Texture coordinates
-    glBindBuffer( GL_ARRAY_BUFFER, VBO[ 2 ] );
-    glBufferData( GL_ARRAY_BUFFER, sizeof( Vector2 ) *texCoords.size( ), texCoords.data( ), GL_STATIC_DRAW );
-    glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, 0, 0 );
-    glEnableVertexAttribArray( 2 );
-
-    //Strips
-    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, VBO[ 3 ] );
-    glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( GLushort ) *indices.size( ), indices.data( ), GL_STATIC_DRAW );
-
-    glBindVertexArray( 0 );
-  }
-
-  void MeshPrimitive::render( void )
-  {
-    glBindVertexArray( VAO );
-
-    glDrawElements( GL_TRIANGLES, sizeof( GLushort ) *indices.size( ),
-      GL_UNSIGNED_SHORT, 0 );
-
-    glBindVertexArray( 0 );
+    MAXPOINTS = vertices.size();
   }
 }
--- mb/Primitives/TorusPrimitive.hpp
+++ mb/Primitives/TorusPrimitive.hpp
@@ -27,10 +27,11 @@ namespace mb
   class TorusPrimitive: public Primitive
   {
   public:
-      TorusPrimitive( float outerRadius = 1.0f, float innerRadius = 0.5f,
-                      unsigned int sides = 4, unsigned int rings = 10,
-                      Primitive::Type type = Primitive::Type::TRIANGLES,
-                      Primitive::TDrawType typeDraw = Primitive::DRAW_ELEMENTS );
+    MB_API
+    TorusPrimitive( float outerRadius = 1.0f, float innerRadius = 0.5f,
+                    unsigned int sides = 4, unsigned int rings = 10,
+                    Primitive::Type type = Primitive::Type::TRIANGLES,
+                    Primitive::TDrawType typeDraw = Primitive::DRAW_ELEMENTS );
 
   };
 }
