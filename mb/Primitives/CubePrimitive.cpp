282420bc4738eb336940dbd830b1accd7cd42aca
rjarillo <r.jarillo@alumnos.urjc.es>
2017-07-19 12:16:19 +0200

    Creating geometry to Cone and Cylinder primitives.
      + Adding minor changes.

--- mb/Primitives/CubePrimitive.cpp
+++ mb/Primitives/CubePrimitive.cpp
@@ -19,126 +19,127 @@
 
 #include "CubePrimitive.hpp"
 
-#include "../Includes.hpp"
-
 namespace mb
 {
-  CubePrimitive::CubePrimitive(float side , Type type, TDrawType typeDraw)
-  : Primitive( type, typeDraw )
-  {
-    float side2 = side / 2.0f;
+    CubePrimitive::CubePrimitive( float side, Primitive::Type type,
+                                  Primitive::TDrawType typeDraw )
+    : Primitive( type, typeDraw )
+    {
+        float side2 = side / 2.0f;
 
-  // Vertices
-  vertices = std::vector<Vector3> {
-    // Front
-    Vector3( -side2, -side2, side2 ),
-    Vector3( side2, -side2, side2 ),
-    Vector3( side2,  side2, side2 ),
-    Vector3( -side2,  side2, side2 ),
-    // Right
-    Vector3( side2, -side2, side2 ),
-    Vector3( side2, -side2, -side2 ),
-    Vector3( side2,  side2, -side2 ),
-    Vector3( side2,  side2, side2 ),
-    // Back
-    Vector3( -side2, -side2, -side2 ),
-    Vector3( -side2,  side2, -side2 ),
-    Vector3( side2,  side2, -side2 ),
-    Vector3( side2, -side2, -side2 ),
-    // Left
-    Vector3( -side2, -side2, side2 ),
-    Vector3( -side2,  side2, side2 ),
-    Vector3( -side2,  side2, -side2 ),
-    Vector3( -side2, -side2, -side2 ),
-    // Bottom
-    Vector3( -side2, -side2, side2 ),
-    Vector3( -side2, -side2, -side2 ),
-    Vector3( side2, -side2, -side2 ),
-    Vector3( side2, -side2, side2 ),
-    // Top
-    Vector3( -side2,  side2, side2 ),
-    Vector3( side2,  side2, side2 ),
-    Vector3( side2,  side2, -side2 ),
-    Vector3( -side2,  side2, -side2 )
-  };
+      // Vertices
+      vertices = std::vector<Vector3> {
+        // Front
+        Vector3( -side2, -side2, side2 ),
+        Vector3( side2, -side2, side2 ),
+        Vector3( side2,  side2, side2 ),
+        Vector3( -side2,  side2, side2 ),
+        // Right
+        Vector3( side2, -side2, side2 ),
+        Vector3( side2, -side2, -side2 ),
+        Vector3( side2,  side2, -side2 ),
+        Vector3( side2,  side2, side2 ),
+        // Back
+        Vector3( -side2, -side2, -side2 ),
+        Vector3( -side2,  side2, -side2 ),
+        Vector3( side2,  side2, -side2 ),
+        Vector3( side2, -side2, -side2 ),
+        // Left
+        Vector3( -side2, -side2, side2 ),
+        Vector3( -side2,  side2, side2 ),
+        Vector3( -side2,  side2, -side2 ),
+        Vector3( -side2, -side2, -side2 ),
+        // Bottom
+        Vector3( -side2, -side2, side2 ),
+        Vector3( -side2, -side2, -side2 ),
+        Vector3( side2, -side2, -side2 ),
+        Vector3( side2, -side2, side2 ),
+        // Top
+        Vector3( -side2,  side2, side2 ),
+        Vector3( side2,  side2, side2 ),
+        Vector3( side2,  side2, -side2 ),
+        Vector3( -side2,  side2, -side2 )
+      };
 
-  MAXPOINTS = vertices.size();
+      MAXPOINTS = vertices.size();
 
-  // Normals
-  normals = std::vector<Vector3> {
-    // Front
-    Vector3( 0.0, 0.0, 1.0 ),
-    Vector3( 0.0, 0.0, 1.0 ),
-    Vector3( 0.0, 0.0, 1.0 ),
-    Vector3( 0.0, 0.0, 1.0 ),
-    // Right
-    Vector3( 1.0, 0.0, 0.0 ),
-    Vector3( 1.0, 0.0, 0.0 ),
-    Vector3( 1.0, 0.0, 0.0 ),
-    Vector3( 1.0, 0.0, 0.0 ),
-    // Back
-    Vector3( 0.0, 0.0, -1.0 ),
-    Vector3( 0.0, 0.0, -1.0 ),
-    Vector3( 0.0, 0.0, -1.0 ),
-    Vector3( 0.0, 0.0, -1.0 ),
-    // Left
-    Vector3( -1.0, 0.0, 0.0 ),
-    Vector3( -1.0, 0.0, 0.0 ),
-    Vector3( -1.0, 0.0, 0.0 ),
-    Vector3( -1.0, 0.0, 0.0 ),
-    // Bottom
-    Vector3( 0.0, -1.0, 0.0 ),
-    Vector3( 0.0, -1.0, 0.0 ),
-    Vector3( 0.0, -1.0, 0.0 ),
-    Vector3( 0.0, -1.0, 0.0 ),
-    // Top
-    Vector3( 0.0, 1.0, 0.0 ),
-    Vector3( 0.0, 1.0, 0.0 ),
-    Vector3( 0.0, 1.0, 0.0 ),
-    Vector3( 0.0, 1.0, 0.0 )
-  };
-  // TexCoords
-  texCoords = std::vector<Vector2> {
-    // Front
-    Vector2( 0.0, 0.0 ),
-    Vector2( 1.0, 0.0 ),
-    Vector2( 1.0, 1.0 ),
-    Vector2( 0.0, 1.0 ),
-    // Right
-    Vector2( 0.0, 0.0 ),
-    Vector2( 1.0, 0.0 ),
-    Vector2( 1.0, 1.0 ),
-    Vector2( 0.0, 1.0 ),
-    // Back
-    Vector2( 0.0, 0.0 ),
-    Vector2( 1.0, 0.0 ),
-    Vector2( 1.0, 1.0 ),
-    Vector2( 0.0, 1.0 ),
-    // Left
-    Vector2( 0.0, 0.0 ),
-    Vector2( 1.0, 0.0 ),
-    Vector2( 1.0, 1.0 ),
-    Vector2( 0.0, 1.0 ),
-    // Bottom
-    Vector2( 0.0, 0.0 ),
-    Vector2( 1.0, 0.0 ),
-    Vector2( 1.0, 1.0 ),
-    Vector2( 0.0, 1.0 ),
-    // Top
-    Vector2( 0.0, 0.0 ),
-    Vector2( 1.0, 0.0 ),
-    Vector2( 1.0, 1.0 ),
-    Vector2( 0.0, 1.0 )
-  };
-  // Indices
-  indices = std::vector< short > {
-    0, 1, 2, 0, 2, 3,
-    4, 5, 6, 4, 6, 7,
-    8, 9, 10, 8, 10, 11,
-    12, 13, 14, 12, 14, 15,
-    16, 17, 18, 16, 18, 19,
-    20, 21, 22, 20, 22, 23
-  };
+      // Normals
+      normals = std::vector<Vector3> {
+        // Front
+        Vector3( 0.0, 0.0, 1.0 ),
+        Vector3( 0.0, 0.0, 1.0 ),
+        Vector3( 0.0, 0.0, 1.0 ),
+        Vector3( 0.0, 0.0, 1.0 ),
+        // Right
+        Vector3( 1.0, 0.0, 0.0 ),
+        Vector3( 1.0, 0.0, 0.0 ),
+        Vector3( 1.0, 0.0, 0.0 ),
+        Vector3( 1.0, 0.0, 0.0 ),
+        // Back
+        Vector3( 0.0, 0.0, -1.0 ),
+        Vector3( 0.0, 0.0, -1.0 ),
+        Vector3( 0.0, 0.0, -1.0 ),
+        Vector3( 0.0, 0.0, -1.0 ),
+        // Left
+        Vector3( -1.0, 0.0, 0.0 ),
+        Vector3( -1.0, 0.0, 0.0 ),
+        Vector3( -1.0, 0.0, 0.0 ),
+        Vector3( -1.0, 0.0, 0.0 ),
+        // Bottom
+        Vector3( 0.0, -1.0, 0.0 ),
+        Vector3( 0.0, -1.0, 0.0 ),
+        Vector3( 0.0, -1.0, 0.0 ),
+        Vector3( 0.0, -1.0, 0.0 ),
+        // Top
+        Vector3( 0.0, 1.0, 0.0 ),
+        Vector3( 0.0, 1.0, 0.0 ),
+        Vector3( 0.0, 1.0, 0.0 ),
+        Vector3( 0.0, 1.0, 0.0 )
+      };
+      // TexCoords
+      texCoords = std::vector<Vector2> {
+        // Front
+        Vector2( 0.0, 0.0 ),
+        Vector2( 1.0, 0.0 ),
+        Vector2( 1.0, 1.0 ),
+        Vector2( 0.0, 1.0 ),
+        // Right
+        Vector2( 0.0, 0.0 ),
+        Vector2( 1.0, 0.0 ),
+        Vector2( 1.0, 1.0 ),
+        Vector2( 0.0, 1.0 ),
+        // Back
+        Vector2( 0.0, 0.0 ),
+        Vector2( 1.0, 0.0 ),
+        Vector2( 1.0, 1.0 ),
+        Vector2( 0.0, 1.0 ),
+        // Left
+        Vector2( 0.0, 0.0 ),
+        Vector2( 1.0, 0.0 ),
+        Vector2( 1.0, 1.0 ),
+        Vector2( 0.0, 1.0 ),
+        // Bottom
+        Vector2( 0.0, 0.0 ),
+        Vector2( 1.0, 0.0 ),
+        Vector2( 1.0, 1.0 ),
+        Vector2( 0.0, 1.0 ),
+        // Top
+        Vector2( 0.0, 0.0 ),
+        Vector2( 1.0, 0.0 ),
+        Vector2( 1.0, 1.0 ),
+        Vector2( 0.0, 1.0 )
+      };
+      // Indices
+      indices = std::vector< short > {
+        0, 1, 2, 0, 2, 3,
+        4, 5, 6, 4, 6, 7,
+        8, 9, 10, 8, 10, 11,
+        12, 13, 14, 12, 14, 15,
+        16, 17, 18, 16, 18, 19,
+        20, 21, 22, 20, 22, 23
+      };
 
-  }
-}
+    }
+
+} // NS
+
--- mb/Primitives/CylinderPrimitive.cpp
+++ mb/Primitives/CylinderPrimitive.cpp
@@ -1,39 +1,37 @@
 /**
  * Copyright (c) 2017, Monkey Brush
  * All rights reserved.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  **/
 
-#ifndef __MB_CONE_PRIMTIIVE__
-#define __MB_CONE_PRIMTIIVE__
+#include "CylinderPrimitive.hpp"
 
-#include "Primitive.hpp"
+#include "../Includes.hpp"
 
 namespace mb
 {
-  class ConePrimitive: public Primitive
-  {
-  public:
-      MB_API
-      ConePrimitive( float bottomRadius, float topRadius,
-                     float height, int radialSubDiv = 12, int heightSubDiv = 1,
-                     bool createTopBase = true, bool createBottomBase = true,
-                     Primitive::Type type = Primitive::Type::TRIANGLES,
-                     Primitive::TDrawType typeDraw = Primitive::DRAW_ELEMENTS );
-  };
-}
+    CylinderPrimitive::CylinderPrimitive(
+                       float radius, float height, float radialSubDiv,
+                       float heightSubDiv, bool createTopBase,
+                       bool createBottomBase,
+                       Primitive::Type type,
+                       Primitive::TDrawType typeDraw )
+    : ConePrimitive( radius, radius, height, radialSubDiv, heightSubDiv,
+                     createTopBase, createBottomBase, type, typeDraw )
+    {
+    }
+} // NS
 
-#endif /* __MB_CONE_PRIMTIIVE__ */
